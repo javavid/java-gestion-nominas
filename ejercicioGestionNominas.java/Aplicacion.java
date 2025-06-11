eca21101085f3699a1344ba9a3ba0bba1e3cb83b
Eder David Jimenez Tpias <ederjimeneztapias@gmail.com>
2025-06-11 17:12:28 -0500

    en la clase principal se realisa el agrgado de un trabajador y se visualiza con el toString recorrido co forech

--- ejercicioGestionNominas.java/Aplicacion.java
+++ ejercicioGestionNominas.java/Aplicacion.java
@@ -3,5 +3,10 @@ public class Aplicacion {
     public static void main(String[] args) {
         Empresa empresa = new Empresa(315636942, "safato");
         empresa.agregarTrabajador(new Analista("fernando", 1039545302, 1000000, 2000000));
+        
+
+        for (Trabajador trabajador : empresa.getJob()) {
+            System.out.println(trabajador);
+        }
     }
 }
