4380f7cd97123e975b3bb80c6897e30e15a890c5
Eder David Jimenez Tpias <ederjimeneztapias@gmail.com>
2025-06-25 15:44:27 -0500

    trabajo terminado

--- ejercicioGestionNominas.java/Aplicacion.java
+++ ejercicioGestionNominas.java/Aplicacion.java
@@ -4,7 +4,16 @@ public class Aplicacion {
         Empresa empresa = new Empresa(315636942, "safato");
         
         empresa.agregarTrabajador(new Analista("fernando", 1039545302, 1000000, "Analista"));
-        
+        empresa.agregarTrabajador(new Analista("sharyt", 1039545203, 5355000, "Analista"));
+
+        empresa.agregarTrabajador(new Programador("luisa", 2047642595, 4000000, "ingeniera"));
+        empresa.agregarTrabajador(new Programador("tany", 23485678, 40000000, "ingeniera"));
+
+        empresa.agregarTrabajador(new Administrativo("lau", 18376452, 4000000, 7));
+        empresa.agregarTrabajador(new Administrativo("pedro", 98765345, 4555000, 3));
+
+        empresa.agregarTrabajador(new Auxiliar("tato", 1286534, 4444000, 4));
+        empresa.agregarTrabajador(new Auxiliar("cami", 283949584, 3976000, 7));
 
         for (Trabajador trabajador : empresa.getJob()) {
             System.out.println(trabajador);
