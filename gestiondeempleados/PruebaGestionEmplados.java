374d584a1a696c961ad61e81b56e970e3f112345
Eder David Jimenez Tpias <ederjimeneztapias@gmail.com>
2025-07-12 22:48:05 -0500

    se actualiza orden en la clase principal el main primero el for y luego el try-catch

--- gestiondeempleados/PruebaGestionEmplados.java
+++ gestiondeempleados/PruebaGestionEmplados.java
@@ -9,14 +9,16 @@ public class PruebaGestionEmplados {
         em.agregarEmpleado(new EmpleadoFijo("Marcela", "garces", "4321", 1444000, 1444000));
         em.agregarEmpleado(new EmpleadoFijo("Pablo", "palmar", "5678", 1555000, 1555000));
 
-        try {
-            em.eliminirEmpleado("1234");
-        } catch (EmpleadoNoEncontradoException e) {
-            System.out.println("Error " + e);
-        }
         for (Empleado emp : em.getEmpleado()) {
             System.out.println(" ");
             System.out.println(emp);
         }
+
+        try {
+            em.eliminirEmpleado("1122");
+        } catch (EmpleadoNoEncontradoException e) {
+            System.out.println("Error " + e);
+        }
+        
     }
 }
