faf06d1e2a10eb46c15573fe8cec43d7abc451e6
Eder David Jimenez Tpias <ederjimeneztapias@gmail.com>
2025-09-20 18:18:32 -0500

    se modifica la clase PrintWriteEjemplo.java

--- Archivos_o_File/PrintWriteEjemplo.java
+++ Archivos_o_File/PrintWriteEjemplo.java
@@ -4,7 +4,6 @@ import java.io.PrintWriter;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class PrintWriteEjemplo {
