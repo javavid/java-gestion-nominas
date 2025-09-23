72c5bfac87eac069a315317a7649dad1225ae379
Eder David Jimenez Tpias <ederjimeneztapias@gmail.com>
2025-09-22 21:02:04 -0500

    se realiza la culminacion de bases de datos en java y se inicia GUI interface grafica de usuario

--- Connector_MySQL/AddImagenes.java
+++ Connector_MySQL/AddImagenes.java
@@ -8,10 +8,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 public class AddImagenes {
-    public static final String NOMBRE = "Abigail";
-    public static final String CORREO = "Abigail@gmail.com";
-    public static final int TEL = 32063250;
+    public static final String NOMBRE = "jocelyn";
+    public static final String CORREO = "jocelyn@gmail.com";
+    public static final int TEL = 32063260;
     public static final String IMAGEN = "C:\\java-gestion-nominas\\Connector_MySQL\\java-151343_1920.png";
 
     public static void main(String[] args) {
@@ -24,10 +26,13 @@ public class AddImagenes {
 
             final byte[] bitImagen = Files.readAllBytes(Paths.get(IMAGEN));
 
+            //ejemplo de datos encryptados:
+            String telIncryptada = BCrypt.hashpw(CORREO, BCrypt.gensalt());//esta linea de codigo encypta la contraseña como no tengo el campo contraseña hago el ejemplo con el correo.
+
             String query = "INSERT INTO contacto(nombre, correo, telefono, imagen, binarioImagen) VALUES (?,?,?,?,?)";
             PreparedStatement prepereConsulta = conexion.prepareStatement(query);
             prepereConsulta.setString(1, NOMBRE);
-            prepereConsulta.setString(2, CORREO);
+            prepereConsulta.setString(2, telIncryptada);
             prepereConsulta.setInt(3, TEL);
             prepereConsulta.setString(4, IMAGEN);
             prepereConsulta.setBytes(5, bitImagen);
---
+++ GUI/EjemploUi.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import javax.swing.JFrame;
+
+public class EjemploUi {
+    
+    public static void main(String[] args) {
+        
+
+        JFrame ventana = new JFrame("Mi primera ventana en JFrame");
+        ventana.setVisible(true);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
